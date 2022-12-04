7b0c4e836e7ee5cc51345e031095f585a3321e33
cryptovincent <zjose88@gmail.com>
2022-12-04 16:00:33 -0300

    test: Students & Subjects controllers tests

--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -1,20 +1,144 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let mockStudentService = {
+    create: jest.fn((dto) => {
+      return {
+        id: Math.random() * (1000 - 1) + 1,
+        ...dto,
+      };
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        id: id,
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(()=>{
+      return {
+        "Items": [
+          {
+              "RUT": "12345678-9",
+              "firstName": "Juan",
+              "lastName": "Perez",
+              "PROFILE": "PROFILE#12345678-9",
+              "address": "Cynth Av. Santa Isabel, CA.",
+              "dob": "1988-03-17",
+              "id": "88f2105c-c826-40d6-898f-60b04152bf57"
+          }
+        ]
+      }
+    }),
+    findOne: jest.fn((rut)=>{
+      return {
+        "Items": [
+          {
+              "RUT": rut,
+              "firstName": "Juan",
+              "lastName": "Perez",
+              "PROFILE": "PROFILE#12345678-9",
+              "address": "Cynth Av. Santa Isabel, CA.",
+              "dob": "1988-03-17",
+              "id": "88f2105c-c826-40d6-898f-60b04152bf57"
+          }
+        ]
+      }
+    }),
+    remove: jest.fn((id) => {
+      return {
+        id: id
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
       providers: [StudentsService],
-    }).compile();
+    }).overrideProvider(StudentsService)
+      .useValue(mockStudentService)
+      .compile();
 
     controller = module.get<StudentsController>(StudentsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a student', () => {
+    const createStudentDto: CreateStudentDto = {
+      RUT: '12312312-3',
+      PROFILE: 'PROFILE#12312312-3',
+      firstName: 'Joseph',
+      lastName: 'Gabanna',
+      dob: new Date(),
+      address: 'Red St. Savanna, CA.',
+    };
+
+    expect(controller.create(createStudentDto)).resolves.toEqual({ msg: "Item created succesfully" });
+  });
+
+  it('should update a student', () => {
+    const updateStudentDto: UpdateStudentDto = {
+      firstName: 'John',
+      lastName: 'Smith',
+      dob: new Date(),
+      address: '1234 Saron St. Placeville CA:',
+    };
+    const studentRut = "12312312-3";
+
+    expect(controller.update(studentRut, updateStudentDto)).resolves.toEqual({ msg: "Item updated succesfully" });
+    expect(mockStudentService.update).toHaveBeenCalledWith(
+      studentRut,
+      updateStudentDto,
+    );
+  });
+
+  it('should return an array of students', async () => {
+    const result = {
+      "Items": [
+        {
+            "RUT": "12345678-9",
+            "firstName": "Juan",
+            "lastName": "Perez",
+            "PROFILE": "PROFILE#12345678-9",
+            "address": "Cynth Av. Santa Isabel, CA.",
+            "dob": "1988-03-17",
+            "id": "88f2105c-c826-40d6-898f-60b04152bf57"
+        }
+      ]
+    };
+
+    expect(controller.findAll()).toEqual(result);
+  });
+
+  it('should return a student by rut', async () => {
+    const result = {
+      "Items": [
+        {
+            "RUT": "12345678-9",
+            "firstName": "Juan",
+            "lastName": "Perez",
+            "PROFILE": "PROFILE#12345678-9",
+            "address": "Cynth Av. Santa Isabel, CA.",
+            "dob": "1988-03-17",
+            "id": "88f2105c-c826-40d6-898f-60b04152bf57"
+        }
+      ]
+    };
+    const studentRut = "12345678-9";
+
+    expect(controller.findOne(studentRut)).toEqual(result);
+  });
+
+  it('should delete a student by rut', async () => {
+    const studentRut = "12345678-9";
+    expect(controller.remove(studentRut)).resolves.toEqual({ msg: "Item deleted succesfully" });
   });
 });
--- src/subjects/subjects.controller.spec.ts
+++ src/subjects/subjects.controller.spec.ts
@@ -1,15 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateSubjectDto } from './dto/create-subject.dto';
+import { UpdateSubjectDto } from './dto/update-subject.dto';
 import { SubjectsController } from './subjects.controller';
 import { SubjectsService } from './subjects.service';
 
 describe('SubjectsController', () => {
   let controller: SubjectsController;
+  let mockSubjectService = {
+    create: jest.fn((rut, dto) => {
+      return {
+        RUT: rut,
+        ...dto,
+      };
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        id: id,
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(()=>{
+      return {
+        "Items": [
+          {
+              "RUT": "12345678-9",
+              "subjectMark": 5.5,
+              "id": "629ee526-a21f-4308-85ac-86e357f4c7cf",
+              "PROFILE": "SUBJECT#629ee526-a21f-4308-85ac-86e357f4c7cf",
+              "subjectName": "Geography"
+          },
+        ]
+      }
+    }),
+    findOne: jest.fn((rut)=>{
+      return {
+        "Items": [
+          {
+              "RUT": rut,
+              "subjectMark": 5.5,
+              "id": "629ee526-a21f-4308-85ac-86e357f4c7cf",
+              "PROFILE": "SUBJECT#629ee526-a21f-4308-85ac-86e357f4c7cf",
+              "subjectName": "Geography"
+          },
+        ]
+      }
+    }),
+    remove: jest.fn((rut, subjectName) => {
+      return {
+        rut: rut,
+        subjectName: subjectName,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SubjectsController],
       providers: [SubjectsService],
-    }).compile();
+    })
+      .overrideProvider(SubjectsService)
+      .useValue(mockSubjectService)
+      .compile();
 
     controller = module.get<SubjectsController>(SubjectsController);
   });
@@ -17,4 +68,72 @@ describe('SubjectsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a subject', () => {
+    const createSubjectDto: CreateSubjectDto = {
+      PROFILE: 'PROFILE#12345678-9',
+      subjectName: 'Math',
+      subjectMark: '5.5',
+    };
+
+    const rut = '12345678-9';
+
+    expect(controller.create(rut, createSubjectDto)).resolves.toEqual({ msg: "Item created succesfully" });
+  });
+
+  it('should update a student', () => {
+    const updateSubjectDto: UpdateSubjectDto = {
+      subjectName: 'Math',
+      subjectMark: '5.5',
+    };
+    const studentRut = "12345678-9";
+    const subjectName = "Math";
+
+    expect(controller.update(studentRut, subjectName, updateSubjectDto)).resolves.toEqual({ msg: "Item updated succesfully" });
+    expect(mockSubjectService.update).toHaveBeenCalledWith(
+      studentRut,
+      updateSubjectDto,
+      subjectName,
+    );
+  });
+
+  it('should return an array of subjects', async () => {
+    const result = {
+      "Items": [
+        {
+            "RUT": "12345678-9",
+            "subjectMark": 5.5,
+            "id": "629ee526-a21f-4308-85ac-86e357f4c7cf",
+            "PROFILE": "SUBJECT#629ee526-a21f-4308-85ac-86e357f4c7cf",
+            "subjectName": "Geography"
+        },
+      ]
+    };
+
+    expect(controller.findAll()).toEqual(result);
+  });
+
+  it('should return all subjects by a specific rut', async () => {
+    const result = {
+      "Items": [
+        {
+            "RUT": "12345678-9",
+            "subjectMark": 5.5,
+            "id": "629ee526-a21f-4308-85ac-86e357f4c7cf",
+            "PROFILE": "SUBJECT#629ee526-a21f-4308-85ac-86e357f4c7cf",
+            "subjectName": "Geography"
+        },
+      ]
+    };
+    const studentRut = "12345678-9";
+
+    expect(controller.findOne(studentRut)).toEqual(result);
+  });
+
+  it('should delete a subject by rut & subjectName', async () => {
+    const studentRut = "12345678-9";
+    const subjectName = "Math";
+    expect(controller.remove(studentRut, subjectName)).resolves.toEqual({ msg: "Item deleted succesfully" });
+  });
+
 });
